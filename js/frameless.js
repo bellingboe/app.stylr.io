b3bbe15f996429cb765e492ba7ac74d1d70507ad
Brenden <brenden@brenden.me>
2014-04-28 08:56:56 -0500

    adding app web source

---
+++ js/frameless.js
@@ -0,0 +1,155 @@
+window.onfocus = function() { 
+    focusTitlebars(true);
+}
+
+window.onblur = function() { 
+    focusTitlebars(false);
+}
+
+window.onresize = function() {
+    updateContentStyle();
+}
+
+window.onload = function() {
+    
+    var mouseStillDown = false;
+    
+    $("body").on("click", ".footer", function(ev){
+        $(this).children(".descr").slideToggle();
+    });
+    
+    $(".tools").on("mousedown", ".pos-add-x", function(ev){
+        mouseStillDown = true;
+        var new_x = (window.BOXITEM.offset().left - 160) + 1;
+        window.BOXITEM.css({"left": new_x+"px"});
+        setPosDisplay(window.BOXITEM.offset().top-35, new_x);
+    });
+    
+    $(".tools").on("mousedown", ".pos-sub-x", function(ev){
+        mouseStillDown = true;
+        var new_x = (window.BOXITEM.offset().left - 160) - 1;
+        window.BOXITEM.css({"left": new_x+"px"});
+        setPosDisplay(window.BOXITEM.offset().top-35, new_x);
+    });
+    
+    $(".tools").on("mousedown", ".pos-add-y", function(ev){
+        mouseStillDown = true;
+        var new_x = (window.BOXITEM.offset().top - 35) + 1;
+        window.BOXITEM.css({"top": new_x+"px"});
+        setPosDisplay(new_x, window.BOXITEM.offset().left-160);
+    });
+    
+    $(".tools").on("mousedown", ".pos-sub-y", function(ev){
+        mouseStillDown = true;
+        var new_x = (window.BOXITEM.offset().top - 35) - 1;
+        window.BOXITEM.css({"top": new_x+"px"});
+        setPosDisplay(new_x, window.BOXITEM.offset().left-160);
+    });
+    
+    /* ================================================= */
+    
+    $(".tools").on("mousedown", ".size-add-w", function(ev){
+        mouseStillDown = true;
+        var new_x = window.BOXITEM.outerWidth() + 1;
+        window.BOXITEM.css({"width": new_x+"px"});
+        setSizeDisplay(window.BOXITEM.outerHeight(), new_x);
+    });
+    
+    $(".tools").on("mousedown", ".size-add-h", function(ev){
+        mouseStillDown = true;
+        var new_x = window.BOXITEM.outerHeight() + 1;
+        window.BOXITEM.css({"height": new_x+"px"});
+        setSizeDisplay(new_x, window.BOXITEM.outerWidth());
+    });
+    
+    $(".tools").on("mousedown", ".size-sub-w", function(ev){
+        mouseStillDown = true;
+        var new_x = window.BOXITEM.outerWidth() - 1;
+        window.BOXITEM.css({"width": new_x+"px"});
+        setSizeDisplay(window.BOXITEM.outerHeight(), new_x);
+    });
+    
+    $(".tools").on("mousedown", ".size-sub-h", function(ev){
+        mouseStillDown = true;
+        var new_x = window.BOXITEM.outerHeight() - 1;
+        window.BOXITEM.css({"height": new_x+"px"});
+        setSizeDisplay(new_x, window.BOXITEM.outerWidth());
+    });
+    
+    $("#btn_gh").on("click", function(ev){
+        window.location = 'https://github.com/bellingboe/project-fluoro';
+    });
+    
+    $("#btn_new").on("click", function(ev){
+        $(this).fadeOut();
+        $("#btn_gh").fadeOut();
+        
+        $(".top-titlebar").css({"top": "-35px"});
+        $(".top-titlebar-text").addClass("app-title");
+        $(".top-titlebar-close-button").addClass("app-title");
+        $(".top-titlebar").addClass("app-title");
+        $(".top-titlebar-icon").addClass("app-title");
+        
+        $(".scene-text").animate({"left": "160px"}, {queue: false});
+        $(".scene-disc").animate({"left": "160px"}, {queue: false});
+        $(".app-by").animate({"left": "160px"}, {queue: false});
+        $("#scene").animate({"left": "160px"}, {queue: false});
+        
+        $(".scene-text").fadeOut();
+        $(".scene-disc").fadeOut();
+        $(".app-by").fadeOut();
+        $(".tools").fadeIn();
+        $("#scene").fadeOut();
+        
+        $(".top-titlebar").animate({"top": "0px"}, {queue: false});
+        
+        $("#content").animate({"left": "160px"}, {queue: false});
+        $(".tools").animate({"left": "0px"}, {queue: false, complete: function(){
+            $(".footer").fadeIn();
+            $(".footer-top").fadeIn();
+            $("#content").css({"position": "absolute"});
+            var box = $("<div>").hide().appendTo($("#content")).fadeIn();
+            setActiveBox(box);
+        }});
+        
+    });
+    
+    $(".tool-box").on("click", ".title", function(ev){
+        var opened = $(this).parent(".tool-box").hasClass("box-open");
+        
+        if (opened) {
+            $(this).parent(".tool-box").removeClass("box-open");
+        } else {
+            $(this).parent(".tool-box").addClass("box-open");
+        }
+        
+        $(this).siblings(".tool-content").toggle();
+    });
+    
+    $(".top-titlebar-close-button > img").attr("src", "button_close_hover.png");
+    
+    $('#color_prev').ColorPicker({
+	color: '#fff',
+	onShow: function (colpkr) {
+		$(colpkr).fadeIn(500);
+		return false;
+	},
+	onHide: function (colpkr) {
+		$(colpkr).fadeOut(500);
+		return false;
+	},
+	onChange: function (hsb, hex, rgb) {
+            setLayerColor(hex, "#");
+	}
+    });
+
+    updateContentStyle();
+    
+    try {
+	require("nw.gui").Window.get().show();
+    } catch (e) {
+	console.log("[browser app]");
+    }
+
+    
+}
---
+++ js/titlebar.js
@@ -0,0 +1,155 @@
+
+
+function closeWindow() {
+  window.close();
+}
+
+function setActiveBox(b) {
+  window.BOXITEM = b;
+  b.addClass("edit-box");
+
+  setPosDisplay(b.offset().top-35,b.offset().left-160);
+  setSizeDisplay(b.outerHeight(),b.outerWidth());
+  setBGDisplay(b.css('backgroundColor'));
+  setLayerColor(b.css('backgroundColor'));
+}
+
+function setPosDisplay(t,l) {
+  $(".pos-x").html(l);
+  $(".pos-y").html(t);
+}
+
+function setSizeDisplay(h,w) {
+  $(".size-w").html(w);
+  $(".size-h").html(h);
+}
+
+function setBGDisplay(bg) {
+  $(".bg-prev").css("background", bg);
+}
+
+function setLayerColor(hex, hash) {
+  if ("undefined" !== typeof hash) {
+    var color = hash + hex;
+  } else {
+    var color = hex;
+  }
+  
+  $('#color_prev').css('backgroundColor', color);
+  window.BOXITEM.css('backgroundColor', color);
+  $('.bg-text').html(color);
+}
+
+function updateImageUrl(image_id, new_image_url) {
+  var image = document.getElementById(image_id);
+  if (image)
+    image.src = new_image_url;
+}
+
+function createImage(image_id, image_url) {
+  var image = document.createElement("img");
+  image.setAttribute("id", image_id);
+  image.src = image_url;
+  return image;
+}
+
+function createButton(button_id, button_name, normal_image_url,
+                       hover_image_url, click_func) {
+  var button = document.createElement("div");
+  button.setAttribute("class", button_name);
+  var button_img = createImage(button_id, normal_image_url);
+  button.appendChild(button_img);
+  button.onmouseover = function() {
+    updateImageUrl(button_id, hover_image_url);
+  }
+  button.onmouseout = function() {
+    updateImageUrl(button_id, normal_image_url);
+  }
+  button.onclick = click_func;
+  return button;
+}
+
+function focusTitlebars(focus) {
+  return;
+  var bg_color = focus ? "#3a3d3d" : "#7a7c7c";
+    
+  var titlebar = document.getElementById("top-titlebar");
+  if (titlebar) {
+    titlebar.style.backgroundColor = bg_color;
+  }
+}
+
+function addTitlebar(titlebar_name, titlebar_icon_url, titlebar_text) {
+  var titlebar = document.createElement("div");
+  titlebar.setAttribute("id", titlebar_name);
+  titlebar.setAttribute("class", titlebar_name);
+
+  var icon = document.createElement("div");
+  icon.setAttribute("class", titlebar_name + "-icon");
+  icon.appendChild(createImage(titlebar_name + "icon", titlebar_icon_url));
+  titlebar.appendChild(icon);
+
+  var title = document.createElement("div");
+  title.setAttribute("class", titlebar_name + "-text");
+  title.innerText = titlebar_text;
+  titlebar.appendChild(title);
+
+  var closeButton = createButton(titlebar_name + "-close-button",
+                                 titlebar_name + "-close-button",
+                                 "button_close.png",
+                                 "button_close_hover.png",
+                                 closeWindow);
+  titlebar.appendChild(closeButton);
+
+  /*
+  var divider = document.createElement("div");
+  divider.setAttribute("class", titlebar_name + "-divider");
+  titlebar.appendChild(divider);
+  */
+  
+  document.body.appendChild(titlebar);
+  
+  var scene = createImage("scene", "");
+  scene.setAttribute("class", "scene-screen");
+  
+  $("#content").append(scene);
+  
+}
+
+function removeTitlebar(titlebar_name) {
+  var titlebar = document.getElementById(titlebar_name);
+  if (titlebar)
+    document.body.removeChild(titlebar);
+}
+
+function updateContentStyle() {
+  var content = document.getElementById("content");
+  if (!content)
+    return;
+
+  var left = 0;
+  var top = 0;
+  var width = window.outerWidth;
+  var height = window.outerHeight;
+
+  var titlebar = document.getElementById("top-titlebar");
+  if (titlebar) {
+    height -= titlebar.offsetHeight;
+    top += titlebar.offsetHeight;
+  } else {
+    addTitlebar("top-titlebar", "top-titlebar.png", "");
+  }
+/*
+  var contentStyle = "position: absolute; ";
+  if ($(".top-titlebar").hasClass("app-title")) {
+    contentStyle += "left: 160px; ";
+    contentStyle += "right: 0px; ";
+  } else {
+    contentStyle += "left: 0px; ";
+  }
+  
+  content.setAttribute("style", contentStyle);
+  */
+  
+  
+}
